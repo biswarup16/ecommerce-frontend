37779e611d298eacb1db3f589bf606e297af4481
KutubUddinAhmed <kutubahmed25012@gmail.com>
2024-11-06 16:12:44 +0530

    check Product Card

--- components/product-card/ProductCard.tsx
+++ components/product-card/ProductCard.tsx
@@ -1,24 +1,73 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StarIcon } from "lucide-react";
 
-function ProductCard({ product, className }: any) {
+interface VariantProps {
+  id: number;
+  pid: number;
+  color: string;
+  images: string[];
+  stock: number;
+  sizes: string[];
+  price: number;
+  sale_price: number;
+}
+
+interface ProductProps {
+  id: number;
+  product_name: string;
+  product_description: string;
+  product_brand: string;
+  category: string;
+  sub_category: string;
+  variants: VariantProps[];
+}
+
+function ProductCard({
+  product,
+  className,
+}: {
+  product: ProductProps;
+  className?: string;
+}) {
+  const [selectedVariant, setSelectedVariant] = useState(product.variants[0]);
+
+  // Track the cart with unique pids
+  const [addToCartProductArray, setAddToCartProductArray] = useState<number[]>(
+    []
+  );
+
+  // Handle color change
+  const handleColorChange = (color: string) => {
+    const variantColor = product.variants.find((v) => v.color === color);
+    if (variantColor) setSelectedVariant(variantColor);
+  };
+
+  // Handle Add to Cart, ensuring unique pids
+  const handleAddToCart = (pid: number) => {
+    setAddToCartProductArray((prev) => [...prev, pid]);
+  };
+
+  useEffect(() => {
+    console.log("Array of Product IDs:", addToCartProductArray);
+    localStorage.setItem(
+      "ProductsArray",
+      JSON.stringify(addToCartProductArray)
+    );
+  }, [addToCartProductArray]);
+
   return (
     <div
-      className={` rounded max-h-[250px] md:max-h-[400px] md:py-2 lg:py-0 lg:min-h-[450px] lg:w-[280px] border ${className}`}
+      className={`rounded max-h-[300px] w-auto md:max-h-[400px] lg:min-h-[450px] lg:w-auto border ${className}`}
     >
-      {/* Product Image and Star Rating */}
-      <div className="relative md:h-[200px] md:w-[230px] w-[120px] h-[120px] mx-auto md:mt-0 mt-1 lg:h-[250px] lg:w-auto ">
+      <div className="relative mx-auto w-full h-[150px] md:h-[200px] md:w-[230px] lg:h-[200px] lg:w-auto rounded-md">
         <Image
-          src="/products-image/product1.avif"
-          alt="Product Image"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
+          src={selectedVariant.images[0]} // Display first image from the array
+          alt={product.product_name}
+          fill
+          className="rounded object-cover"
         />
-
-        {/* Star Rating */}
-        <div className="absolute bottom-[3px] md:bottom-2 md:left-3 flex items-center md:space-x-2 bg-white bg-opacity-30 backdrop-blur-md px-1 md:px-4 md:py-1.5 lg:py-2 rounded-md border border-gray-200">
+        <div className="absolute bottom-[3px] left-[5px] md:bottom-2 md:left-3 flex items-center bg-white bg-opacity-30 backdrop-blur-md px-1 md:px-4 md:py-1.5 lg:py-2 rounded-md border border-gray-200">
           <span className="text-[6px] md:text-xs lg:text-[14px] font-medium text-gray-700">
             4.5
           </span>
@@ -27,39 +76,63 @@ function ProductCard({ product, className }: any) {
         </div>
       </div>
 
-      {/* Product Info */}
-      <div className="w-full  p-2 md:px-1.5 md:pt-1 lg:px-3 lg:py-1   space-y-1 lg:space-y-2 flex flex-col items-start h-[150px] lg:h-[200px] overflow-hidden">
-        {/* Product name and details */}
-        <div className="flex flex-col items-start ">
-          <span className="text-black font-semibold text-sm md:text-lg lg:text-2xl ">
-            Product Name
+      <div className="p-3 flex flex-col md:justify-between h-[200px] lg:h-[250px] overflow-hidden gap-2 md:space-y-2">
+        <div className="flex flex-col items-start">
+          <span className="text-black/90 font-semibold text-sm md:text-lg lg:text-xl line-clamp-2">
+            {product.product_brand}
           </span>
-          <p className="text-gray-600 text-[8px] md:text-xs lg:text-[14px]">
-            A brief description of the product goes here.
-          </p>
+          <div className="text-gray-600 text-[8px] md:text-xs lg:text-[13px] line-clamp-2 ">
+            {product.product_name.split(" ").slice(0, 4).join(" ") + " ..."}
+          </div>
         </div>
 
-        {/* Price Section */}
-        <div className="flex items-center justify-center md:space-x-2 space-x-2">
-          <span className="text-black font-semibold text-[12px] md:text-[16px] lg:text-lg">
-            ₹999.99
+        <div className="flex items-center space-x-2">
+          <span className="text-black font-semibold text-[12px] md:text-[16px] lg:text-md">
+            ₹{selectedVariant.price}
           </span>
-          <span className="text-red-700 line-through text-[8px] sm:text-xs lg:text-[16px]">
-            ₹149.99
+          <span className="text-red-700 line-through text-[8px] sm:text-xs lg:text-[14px]">
+            ₹{selectedVariant.sale_price}
           </span>
         </div>
 
-        {/* Size Selection */}
-        <div className="flex items-center space-x-1 text-xs lg:text-lg">
-          <p className="text-gray-900 font-semibold">Size:</p>
-          <p className="">S</p>
-        </div>
-        {/* Add to Cart Button */}
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center space-x-1 text-xs lg:text-sm">
+            <span className="text-gray-900 font-semibold">Size:</span>
+            <select
+              className="w-18 h-6 md:w-20 md:h-8 lg:w-12 lg:h-7 border border-gray-300 rounded text-xs"
+              defaultValue={selectedVariant.sizes[0]}
+            >
+              {selectedVariant.sizes.map((size, index) => (
+                <option
+                  key={index}
+                  value={size}
+                  className="text-xs text-center"
+                >
+                  {size}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className="w-full text-center">
+          <div className="flex space-x-1">
+            {product.variants?.map((variant, index) => (
+              <span
+                key={index}
+                className={`w-3 h-3 md:w-4 md:h-4 rounded-full border border-gray-300 cursor-pointer ${
+                  selectedVariant.color === variant.color ? "border-black" : ""
+                }`}
+                style={{ backgroundColor: variant.color.toLowerCase() }}
+                onClick={() => handleColorChange(variant.color)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="w-full text-center mt-2">
           <button
+            onClick={() => handleAddToCart(selectedVariant.pid)}
             type="button"
-            className=" md:py-2 lg:py-3 text-xs rounded p-[5px] text-white bg-black w-[70%]"
+            className="md:py-2 lg:py-3 lg:px-2 text-xs rounded p-[5px] text-white bg-black w-[70%]"
           >
             Add To Cart
           </button>
--- components/product-list/ProductList.tsx
+++ components/product-list/ProductList.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { useState } from "react";
 import ProductCard from "../product-card/ProductCard";
 import {
   Select,
@@ -8,48 +10,119 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { ProductData } from "@/utils/ProductData.js";
 
 function ProductList() {
-  const categories = ["All", "Shoes", "Casual", "T-shirt", "Pants"];
-  const sortOptions = ["Relevance", "Price: Low to High", "Price: High to Low"];
-  const SizeOption = ["All", "XS", "S", "M", "L", "XL"];
-  const colorOptions = [
-    { name: "Red", code: "#FF0000" },
-    { name: "Green", code: "#00FF00" },
-    { name: "Blue", code: "#0000FF" },
-    { name: "Yellow", code: "#FFFF00" },
-    { name: "Purple", code: "#800080" },
-  ];
-  // Placeholder product data
-  const products = [
-    { id: 1, name: "Product 1", price: "99.99" },
-    { id: 2, name: "Product 2", price: "149.99" },
-    { id: 3, name: "Product 3", price: "79.99" },
-    { id: 4, name: "Product 4", price: "79.99" },
-    { id: 5, name: "Product 5", price: "79.99" },
-    { id: 6, name: "Product 6", price: "79.99" },
-    { id: 7, name: "Product 7", price: "79.99" },
-    { id: 8, name: "Product 8", price: "79.99" },
-    { id: 9, name: "Product 5", price: "79.99" },
-    { id: 10, name: "Product 6", price: "79.99" },
-    { id: 11, name: "Product 7", price: "79.99" },
-    { id: 12, name: "Product 8", price: "79.99" },
+  // Setting up the product data and filter options
+  const [products] = useState(ProductData);
+  const [subCategorySelected, setsubCategorySelected] = useState("");
+
+  // Define options for filters
+  const categories = ["Men", "Women", "Kids"];
+  const subCategories = ["T-shirts", "Shirts", "Trouser", "Footwares"];
+  const sortOptions = ["Price: Low to High", "Price: High to Low"];
+  const SizeOption = [
+    {
+      type: "Clothing",
+      sizes: ["XS", "S", "M", "L", "XL"],
+    },
+    {
+      type: "Footwares",
+      sizes: ["UK6", "UK7", "UK8", "UK9", "UK10"],
+    },
   ];
 
+  const colorOptions = [
+    { name: "Blue" },
+    { name: "Red" },
+    { name: "Black" },
+    { name: "White" },
+    { name: "Green" },
+    // Add more colors as needed
+  ];
+
+  // Conditional Sizes options
+  function getSizeOption(): string[] {
+    // If no sub-category is selected
+    if (!subCategorySelected) {
+      return SizeOption.flatMap((option) => option.sizes);
+    }
+
+    return subCategorySelected === "Footwares"
+      ? // If footware selected
+        SizeOption.find((option) => option.type === "Footwares")?.sizes || []
+      : // If clothing selected
+        SizeOption.find((option) => option.type === "Clothing")?.sizes || [];
+  }
+
+  const [categorySelected, setCategorySelected] = useState("");
+  const [colorSelected, setColorSelected] = useState("");
+  const [sortSelected, setSortSelected] = useState("");
+  const [sizeSelected, setSizeSelected] = useState("");
+
+  // Filtering Logic
+  function getFilterProduct() {
+    return (
+      products
+        .filter((product) =>
+          // Category
+          categorySelected ? product.category === categorySelected : true
+        )
+        // Sub Category
+        .filter((product) =>
+          subCategorySelected
+            ? product.sub_category === subCategorySelected
+            : true
+        )
+        // color
+        .filter((product) =>
+          colorSelected
+            ? product.variants.some((v) => v.color === colorSelected)
+            : true
+        )
+
+        // Sizes
+        .filter((product) =>
+          sizeSelected
+            ? product.variants.some((s) => s.sizes.includes(sizeSelected))
+            : true
+        )
+
+        // // Sorting
+        .sort((a, b) => {
+          if (sortSelected === "Price: Low to High") {
+            const priceA = a.variants.find((p) => p.price)?.price ?? 0;
+            const priceB = b.variants.find((p) => p.price)?.price ?? 0;
+            return priceA - priceB;
+          }
+          if (sortSelected === "Price: High to Low") {
+            const priceA = a.variants.find((p) => p.price)?.price ?? 0;
+            const priceB = b.variants.find((p) => p.price)?.price ?? 0;
+            return priceB - priceA;
+          }
+          return 0;
+        })
+    );
+  }
+
   return (
-    <div className="min-h-screen text-black px-6 py-1">
+    <div className=" lg:max-w-[90rem] mx-auto text-black px-6 py-1">
       {/* Filters Section */}
       <section className="mb-4">
         <h2 className="text-2xl font-semibold mb-2">Filters</h2>
         <div className="flex flex-wrap gap-2 md:gap-4">
           {/* Category Selector */}
-          <Select>
-            <SelectTrigger className="w-[180px]  border border-black/60 text-black rounded p-4">
-              <SelectValue placeholder="Select Category" />
+          <Select
+            onValueChange={(value) =>
+              setCategorySelected(value === "none" ? "" : value)
+            }
+          >
+            <SelectTrigger className="w-[180px] border border-black/60 text-black rounded p-4">
+              <SelectValue placeholder="Category" />
             </SelectTrigger>
             <SelectContent className="bg-black text-white">
               <SelectGroup>
-                <SelectLabel className="text-gray-300">Categories</SelectLabel>
+                <SelectItem value="none">Categories</SelectItem>
                 {categories.map((category) => (
                   <SelectItem key={category} value={category}>
                     {category}
@@ -58,17 +131,39 @@ function ProductList() {
               </SelectGroup>
             </SelectContent>
           </Select>
+          {/* Sub Category Selector */}
+          <Select
+            onValueChange={(value) =>
+              setsubCategorySelected(value === "none" ? "" : value)
+            }
+          >
+            <SelectTrigger className="w-[180px] border border-black/60 text-black rounded p-4">
+              <SelectValue placeholder="Sub Category" />
+            </SelectTrigger>
+            <SelectContent className="bg-black text-white">
+              <SelectGroup>
+                <SelectItem value="none">Sub-Categories</SelectItem>
+                {subCategories.map((sub) => (
+                  <SelectItem key={sub} value={sub}>
+                    {sub}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
 
           {/* Sort Selector */}
-          <Select>
-            <SelectTrigger className="w-[180px]  border border-black/60 text-black rounded p-4">
+          <Select
+            onValueChange={(value) =>
+              setSortSelected(value === "none" ? "" : value)
+            }
+          >
+            <SelectTrigger className="w-[180px] border border-black/60 text-black rounded p-4">
               <SelectValue placeholder="Sort By" />
             </SelectTrigger>
             <SelectContent className="bg-black text-white">
               <SelectGroup>
-                <SelectLabel className="text-gray-300">
-                  Sort Options
-                </SelectLabel>
+                <SelectItem value="none">Sort Options</SelectItem>
                 {sortOptions.map((option) => (
                   <SelectItem key={option} value={option}>
                     {option}
@@ -77,40 +172,48 @@ function ProductList() {
               </SelectGroup>
             </SelectContent>
           </Select>
+
           {/* Size Selector */}
-          <Select>
-            <SelectTrigger className="w-[180px]  border border-black/60 text-black rounded p-4">
+          <Select
+            onValueChange={(value) =>
+              setSizeSelected(value === "none" ? "" : value)
+            }
+          >
+            <SelectTrigger className="w-[180px] border border-black/60 text-black rounded p-4">
               <SelectValue placeholder="Sizes" />
             </SelectTrigger>
             <SelectContent className="bg-black text-white">
               <SelectGroup>
-                <SelectLabel className="text-gray-300">Sizes</SelectLabel>
-                {SizeOption.map((option) => (
-                  <SelectItem key={option} value={option}>
-                    {option}
+                <SelectItem value="none">Sizes</SelectItem>
+                {getSizeOption().map((size) => (
+                  <SelectItem key={size} value={size}>
+                    {size}
                   </SelectItem>
                 ))}
               </SelectGroup>
             </SelectContent>
           </Select>
+
           {/* Color Selector */}
-          <Select>
+          <Select
+            onValueChange={(value) =>
+              setColorSelected(value === "none" ? "" : value)
+            }
+          >
             <SelectTrigger className="w-[180px] border border-black/60 text-black rounded p-4">
               <SelectValue placeholder="Select Color" />
             </SelectTrigger>
             <SelectContent className="bg-white text-black">
               <SelectGroup>
-                <SelectLabel className="text-gray-500">Colors</SelectLabel>
+                <SelectItem value="none">Colors</SelectItem>
                 {colorOptions.map((color) => (
                   <SelectItem key={color.name} value={color.name}>
                     <div className="flex items-center justify-between w-[100px]">
                       <span className="text-left">{color.name}</span>
-                      {/* Left-align name */}
                       <span
                         className="w-2 h-2 rounded-full"
-                        style={{ backgroundColor: color.code }}
+                        style={{ backgroundColor: color.name.toLowerCase() }}
                       ></span>
-                      {/* Right-align color dot */}
                     </div>
                   </SelectItem>
                 ))}
@@ -123,8 +226,8 @@ function ProductList() {
       {/* Products Section */}
       <section>
         <h2 className="text-2xl font-semibold mb-4">Products</h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-2">
-          {products.map((product) => (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
+          {getFilterProduct().map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
